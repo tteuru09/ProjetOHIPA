e4fb2b745c2a0ebe470a20db119256cff00bbe41
tteuru09 <toamiriurateururai@gmail.com>
2024-03-17 10:17:43 -1000

    UploadFichierFrontEnd

---
+++ Dev/Frontend/projet-ohipa/src/app/upload/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div className="size-64 p-8" {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+  );
+}
+
+export default MyDropzone;
